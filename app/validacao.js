f8763ea75b7ed492885b0f9dd5211df4d65db41c
Marcos Rezende <marcos.arezende@gmail.com>
2023-12-17 23:14:37 -0300

    adicionado comando game over

--- app/validacao.js
+++ app/validacao.js
@@ -2,6 +2,24 @@ function chutePossuiValorValido() {
     const numero = +chute
 
     if (chuteForInvalido(numero)) {
+
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+            document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
+        return
+    }
+
+    if (chuteForInvalido(numero)) {
         console.log('<div>Valor inválido</div>')
         return
     }
